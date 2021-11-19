effbb8b77d8272b298d7b1fa1baadddaa6fd8444
medivh13 <jody.almaida@gmail.com>
2021-11-19 07:38:48 +0700

    update save mahasiswaalamat and update mahasiswa

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,4 +1,8 @@
 package repository
 
+import "github.com/medivh13/mahasiswaservice/internal/models"
+
 type Repository interface {
+	SaveMahasiswaAlamat(dataMahasiswa *models.MahasiswaModels, dataAlamat []*models.MahasiswaAlamatModels) error
+	UpdateMahasiswaNama(dataMahasiswa *models.MahasiswaModels) error
 }
--- internal/services/services.go
+++ internal/services/services.go
@@ -1,4 +1,8 @@
 package services
 
+import "github.com/medivh13/mahasiswaservice/pkg/dto"
+
 type Services interface {
+	SaveMahasiswaAlamat(req *dto.MahasiswaReqDTO) error
+	UpdateMahasiswaNama(req *dto.UpadeMahasiswaNamaReqDTO) error
 }
